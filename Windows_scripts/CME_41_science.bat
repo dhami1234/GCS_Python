ace1bcd1f7eba61e557b9b327dac29152e2d9b8d
dhami1234 <55599148+dhami1234@users.noreply.github.com>
2024-06-14 16:04:07 -0500

    Windows scripts fixed

--- Windows_scripts/CME_41_science.bat
+++ Windows_scripts/CME_41_science.bat
@@ -1,5 +1,5 @@
-python ../gui.py -rd -soho 'C3' -stereo 'COR2' "2012-10-05 06:24" 'STA' 'SOHO' 'STB'
-python ../gui.py -rd -soho 'C3' -stereo 'COR2' "2012-10-05 06:54" 'STA' 'SOHO' 'STB'
-python ../gui.py -rd -soho 'C3' -stereo 'COR2' "2012-10-05 07:24" 'STA' 'SOHO' 'STB'
-python ../gui.py -rd -soho 'C3' -stereo 'COR2' "2012-10-05 07:39" 'STA' 'SOHO' 'STB'
-python ../gui.py -rd -soho 'C3' -stereo 'COR2' "2012-10-05 07:54" 'STA' 'SOHO' 'STB'
+python ../gui.py -rd -soho C3 -stereo COR2 "2012-10-05 06:24" STA SOHO STB
+python ../gui.py -rd -soho C3 -stereo COR2 "2012-10-05 06:54" STA SOHO STB
+python ../gui.py -rd -soho C3 -stereo COR2 "2012-10-05 07:24" STA SOHO STB
+python ../gui.py -rd -soho C3 -stereo COR2 "2012-10-05 07:39" STA SOHO STB
+python ../gui.py -rd -soho C3 -stereo COR2 "2012-10-05 07:54" STA SOHO STB
